ced4a979c9cc198b5657b03273824c3fb42d41b4
jim <thecastlefaller@gmail.com>
2020-03-13 17:14:03 +0800

    add styling

--- src/App2.tsx
+++ src/App2.tsx
@@ -52,114 +52,13 @@ export function App() {
   }, []);
 
   return (
-    <StyledApp>
+    <div style={{border:'solid 3px black', margin:50}}>
       <StateChartVisualization
         service={service}
         visible={true}
         onSelectService={() => void 0}
         // onReset={onReset}
-      />
-    </StyledApp>
+        />
+    </div>
   );
 }
-
-
-export const StyledLayoutButton = styled.button`
-  appearance: none;
-  background: white;
-  box-shadow: var(--shadow);
-  font-weight: bold;
-  text-transform: uppercase;
-  padding: 0.5rem 1rem;
-  border-top-left-radius: 1rem;
-  border-bottom-left-radius: 1rem;
-  color: black;
-  border: none;
-  position: absolute;
-  top: 0;
-  right: 0;
-  cursor: pointer;
-  z-index: 2;
-  margin-top: 1rem;
-  opacity: 0.7;
-  transition: all var(--duration) var(--easing);
-  transform: translateX(0.5rem);
-
-  &:focus,
-  &:hover {
-    outline: none;
-  }
-
-  &:hover {
-    opacity: 1;
-    transform: none;
-  }
-
-  [data-layout='viz'] & {
-    right: 100%;
-    color: black;
-  }
-`;
-
-
-export const StyledHeader = styled.header`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: stretch;
-  grid-area: header;
-  padding: 0.5rem 1rem;
-  z-index: 1;
-  white-space: nowrap;
-`;
-
-const StyledApp = styled.main`
-  --color-app-background: #fff;
-  --color-border: #c9c9c9;
-  --color-primary: rgba(87, 176, 234, 1);
-  --color-primary-faded: rgba(87, 176, 234, 0.5);
-  --color-primary-shadow: rgba(87, 176, 234, 0.1);
-  --color-link: rgba(87, 176, 234, 1);
-  --color-disabled: #b3b3b3;
-  --color-edge: #c9c9c9;
-  --color-edge-active: var(--color-primary);
-  --color-secondary: rgba(255, 152, 0, 1);
-  --color-secondary-light: rgba(255, 152, 0, 0.5);
-  --color-sidebar: #272722;
-  --color-gray: #555;
-  --color-failure: #ee7170;
-  --color-success: #31ae00;
-  --radius: 0.2rem;
-  --border-width: 2px;
-  --sidebar-width: 25rem;
-  --shadow: 0 0.5rem 1rem var(--shadow-color, rgba(0, 0, 0, 0.2));
-  --duration: 0.2s;
-  --easing: cubic-bezier(0.5, 0, 0.5, 1);
-
-  height: 100%;
-  display: grid;
-  grid-template-areas:
-    'header sidebar'
-    'content content';
-  grid-template-rows: 3rem auto;
-  grid-template-columns: auto var(--sidebar-width);
-  overflow: hidden;
-
-  > ${StyledLayoutButton} {
-    display: inline-block;
-    grid-row: 2;
-    grid-column: -1;
-  }
-
-  @media (max-width: 900px) {
-    grid-template-columns: 50% 50%;
-  }
-
-  &[data-embed] {
-    grid-template-rows: 0 auto;
-
-    > ${StyledHeader} {
-      display: none;
-    }
-  }
-`;
--- src/StateChartVisualization.tsx
+++ src/StateChartVisualization.tsx
@@ -71,7 +71,6 @@ export const StyledStateChartContainer = styled.section`
 
 export const StyledStateChart = styled.div`
   grid-area: content;
-  display: grid;
   grid-template-columns: 1fr var(--sidebar-width, 25rem);
   grid-template-rows: 1fr;
   grid-template-areas: 'content sidebar';
@@ -155,123 +154,223 @@ export const StateChartVisualization: React.SFC<{
   }
 
   return (
-    <StyledStateChart>
-      <StyledVisualization>
-        <svg
-          width="100%"
-          height="100%"
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            // @ts-ignore
-            '--color': 'gray',
-            overflow: 'visible',
-            pointerEvents: 'none'
-          }}
-          ref={svgRef}
-          key={JSON.stringify(state.toggledStates)}
-        >
-          <defs>
-            <marker
-              id="marker"
-              markerWidth="4"
-              markerHeight="4"
-              refX="2"
-              refY="2"
-              markerUnits="strokeWidth"
-              orient="auto"
-            >
-              <path d="M0,0 L0,4 L4,2 z" fill="var(--color-edge)" />
-            </marker>
-            <marker
-              id="marker-preview"
-              markerWidth="4"
-              markerHeight="4"
-              refX="2"
-              refY="2"
-              markerUnits="strokeWidth"
-              orient="auto"
-            >
-              <path d="M0,0 L0,4 L4,2 z" fill="var(--color-edge-active)" />
-            </marker>
-          </defs>
-          {edges.map(edge => {
-            if (!svgRef.current) {
-              return;
-            }
+    <StyledApp>
+      <StyledStateChart>
+        <StyledVisualization>
+          <svg
+            width="100%"
+            height="100%"
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              // @ts-ignore
+              '--color': 'gray',
+              overflow: 'visible',
+              pointerEvents: 'none'
+            }}
+            ref={svgRef}
+            key={JSON.stringify(state.toggledStates)}
+          >
+            <defs>
+              <marker
+                id="marker"
+                markerWidth="4"
+                markerHeight="4"
+                refX="2"
+                refY="2"
+                markerUnits="strokeWidth"
+                orient="auto"
+              >
+                <path d="M0,0 L0,4 L4,2 z" fill="var(--color-edge)" />
+              </marker>
+              <marker
+                id="marker-preview"
+                markerWidth="4"
+                markerHeight="4"
+                refX="2"
+                refY="2"
+                markerUnits="strokeWidth"
+                orient="auto"
+              >
+                <path d="M0,0 L0,4 L4,2 z" fill="var(--color-edge-active)" />
+              </marker>
+            </defs>
+            {edges.map(edge => {
+              if (!svgRef.current) {
+                return;
+              }
 
-            // const svgRect = this.svgRef.current.getBoundingClientRect();
+              // const svgRect = this.svgRef.current.getBoundingClientRect();
 
-            return (
-              <Edge
-                key={serializeEdge(edge)}
-                svg={svgRef.current}
-                edge={edge}
-                preview={
-                  edge.event === state.previewEvent &&
-                  current.matches(edge.source.path.join('.')) &&
-                  !!state.preview &&
-                  state.preview.matches(edge.target.path.join('.'))
-                }
-              />
-            );
-          })}
-          {initialStateNodes(service.machine).map((initialStateNode, i) => {
-            if (!svgRef.current) {
-              return;
-            }
+              return (
+                <Edge
+                  key={serializeEdge(edge)}
+                  svg={svgRef.current}
+                  edge={edge}
+                  preview={
+                    edge.event === state.previewEvent &&
+                    current.matches(edge.source.path.join('.')) &&
+                    !!state.preview &&
+                    state.preview.matches(edge.target.path.join('.'))
+                  }
+                />
+              );
+            })}
+            {initialStateNodes(service.machine).map((initialStateNode, i) => {
+              if (!svgRef.current) {
+                return;
+              }
 
-            return (
-              <InitialEdge
-                key={`${initialStateNode.id}_${i}`}
-                source={initialStateNode}
-                svgRef={svgRef.current}
-                preview={
-                  current.matches(initialStateNode.path.join('.')) ||
-                  (!!state.preview &&
-                    state.preview.matches(initialStateNode.path.join('.')))
-                }
-              />
-            );
-          })}
-        </svg>
-        <StateChartNode
-          stateNode={service.machine}
-          current={service.state}
-          transitionCount={transitionCount}
-          level={0}
-          preview={state.preview}
-          onReset={onReset}
-          onEvent={event => {
-            send(event);
-          }}
-          onPreEvent={event => {
-            if (!state.preview) {
+              return (
+                <InitialEdge
+                  key={`${initialStateNode.id}_${i}`}
+                  source={initialStateNode}
+                  svgRef={svgRef.current}
+                  preview={
+                    current.matches(initialStateNode.path.join('.')) ||
+                    (!!state.preview &&
+                      state.preview.matches(initialStateNode.path.join('.')))
+                  }
+                />
+              );
+            })}
+          </svg>
+          <StateChartNode
+            stateNode={service.machine}
+            current={service.state}
+            transitionCount={transitionCount}
+            level={0}
+            preview={state.preview}
+            onReset={onReset}
+            onEvent={event => {
+              send(event);
+            }}
+            onPreEvent={event => {
+              if (!state.preview) {
+                setState({
+                  ...state,
+                  preview: service.nextState(event),
+                  previewEvent: event
+                });
+              }
+            }}
+            onExitPreEvent={() => {
               setState({
                 ...state,
-                preview: service.nextState(event),
-                previewEvent: event
+                preview: undefined,
+                previewEvent: undefined
               });
-            }
-          }}
-          onExitPreEvent={() => {
-            setState({
-              ...state,
-              preview: undefined,
-              previewEvent: undefined
-            });
-          }}
-          onSelectServiceId={serviceId => {
-            const s = (service as any).children.get(serviceId);
+            }}
+            onSelectServiceId={serviceId => {
+              const s = (service as any).children.get(serviceId);
 
-            if (s) {
-              onSelectService(s); // TODO: pass service via context
-            }
-          }}
-          toggledStates={state.toggledStates}
-        />
-      </StyledVisualization>
-    </StyledStateChart>
+              if (s) {
+                onSelectService(s); // TODO: pass service via context
+              }
+            }}
+            toggledStates={state.toggledStates}
+          />
+        </StyledVisualization>
+      </StyledStateChart>
+    </StyledApp>
   );
 };
+
+
+export const StyledLayoutButton = styled.button`
+  appearance: none;
+  background: white;
+  box-shadow: var(--shadow);
+  font-weight: bold;
+  text-transform: uppercase;
+  padding: 0.5rem 1rem;
+  border-top-left-radius: 1rem;
+  border-bottom-left-radius: 1rem;
+  color: black;
+  border: none;
+  position: absolute;
+  top: 0;
+  right: 0;
+  cursor: pointer;
+  z-index: 2;
+  margin-top: 1rem;
+  opacity: 0.7;
+  transition: all var(--duration) var(--easing);
+  transform: translateX(0.5rem);
+
+  &:focus,
+  &:hover {
+    outline: none;
+  }
+
+  &:hover {
+    opacity: 1;
+    transform: none;
+  }
+
+  [data-layout='viz'] & {
+    right: 100%;
+    color: black;
+  }
+`;
+
+
+export const StyledHeader = styled.header`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: stretch;
+  grid-area: header;
+  z-index: 1;
+  white-space: nowrap;
+`;
+
+const StyledApp = styled.main`
+  --color-app-background: #fff;
+  --color-border: #c9c9c9;
+  --color-primary: rgba(87, 176, 234, 1);
+  --color-primary-faded: rgba(87, 176, 234, 0.5);
+  --color-primary-shadow: rgba(87, 176, 234, 0.1);
+  --color-link: rgba(87, 176, 234, 1);
+  --color-disabled: #b3b3b3;
+  --color-edge: #c9c9c9;
+  --color-edge-active: var(--color-primary);
+  --color-secondary: rgba(255, 152, 0, 1);
+  --color-secondary-light: rgba(255, 152, 0, 0.5);
+  --color-sidebar: #272722;
+  --color-gray: #555;
+  --color-failure: #ee7170;
+  --color-success: #31ae00;
+  --radius: 0.2rem;
+  --border-width: 2px;
+  --sidebar-width: 25rem;
+  --shadow: 0 0.5rem 1rem var(--shadow-color, rgba(0, 0, 0, 0.2));
+  --duration: 0.2s;
+  --easing: cubic-bezier(0.5, 0, 0.5, 1);
+
+  grid-template-areas:
+    'header sidebar'
+    'content content';
+  grid-template-rows: 3rem auto;
+  grid-template-columns: auto var(--sidebar-width);
+  overflow: hidden;
+
+  > ${StyledLayoutButton} {
+    display: inline-block;
+    grid-row: 2;
+    grid-column: -1;
+  }
+
+  @media (max-width: 900px) {
+    grid-template-columns: 50% 50%;
+  }
+
+  &[data-embed] {
+    grid-template-rows: 0 auto;
+
+    > ${StyledHeader} {
+      display: none;
+    }
+  }
+`;
